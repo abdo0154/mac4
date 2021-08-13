61a72a79bae7dd24930a141c16572d51a6b6aac8
abdo0154 <57696090+abdo0154@users.noreply.github.com>
2021-08-13 04:47:55 +0200

    Create 12002.js

---
+++ 12002.js
@@ -0,0 +1,11 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Title Page</title>
+</head>
+<body><iframe allowfullscreen='' frameborder='0' height='100%' src='https://edu.volatilewriter.com/#/hosijiwusovozehicafunusinibimuv/U2FsdGVkX19R5E5nu409A7Ie7ZoH4poe0T2iM5ScP4QEdQqLt' style='border:0px #ffffff none;' width='100%'/></body>
+</html>
